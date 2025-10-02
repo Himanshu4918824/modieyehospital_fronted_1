73dec9c26cb490bbe4e8f16f0a9c677d5d0b1b3f
Himanshu4918824 <hs4918824@gmail.com>
2025-10-02 19:22:12 +0530

    add Patient

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,12 @@ import Report from "./components/admin/forms/Report";
 import Doctor from "./components/admin/forms/Doctor";
 import Charges from "./components/admin/forms/Charges";
 import Payment from "./components/admin/forms/Payment";
+import ShowPatientDetails from "./components/admin/homepage/ShowPatientDetails";
 
 function App() {
 
   return (
-  <div className="px-3">
+  <div>
     <Router>
       <Routes>
         <Route element={<DoctorConcern/>} path="/doctorconcern"></Route>
@@ -58,6 +59,9 @@ function App() {
         <Route element={<Doctor/>} path="/doctor"></Route>
         <Route element={<Charges/>} path="/charge"></Route>
         <Route element={<Payment/>} path="/payment"></Route>
+
+
+        <Route element={<ShowPatientDetails/>} path="/showpatient"></Route>
       </Routes>
     </Router>
   </div>)
