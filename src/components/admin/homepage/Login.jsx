6d127ab408647c24a034e72f5cd3a8c7f5013e04
kartikgupta666 <kartikgangil@gmail.com>
2025-10-10 23:31:29 +0530

    continue

--- src/components/admin/homepage/Login.jsx
+++ src/components/admin/homepage/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const result = await axios.post('httpṣ://doctor-backend.up.railway.app/api/v1/login', {
+      const result = await axios.post('https://doctor-backend.up.railway.app/api/v1/login', {
         email, password
       })
       console.log(result)
